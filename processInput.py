827c9796914fd99ab9c1bfa5bcc730c640818cc6
beamisha <beamish.alex@gmail.com>
2022-03-30 16:27:47 -0400

    Update processInput.py

--- processInput.py
+++ processInput.py
@@ -1,18 +1,11 @@
 import data
 import sentenceProcessor
+import nltk
+from sentence_transformers import SentenceTransformer
 
-
-similarityDic = {}
-actions = []
-ls1 = []
-ls1.append('loadTruck')
-ls1.append('changeColor')
-ls1.append('movecurbtocurb')
-ls1.append('untrap')
-ls1.append('LIGHT_MATCH')
-ls1.append('lift')
-ls1.append('movevehicleroad')
-ls1.append('switchon')
+num_actions = 7
+names = ['loadTruck', 'changeColor', 'movecurbtocurb', 'untrap', 'LIGHT_MATCH', 'lift', 'movevehicleroad', 'switchon']
+durationWords = ['duration', 'takes', 'lasts']
 
 # Removes the parenthesese and question marks from a line of PDDL code,
 # and returns the modified line of code.
@@ -48,17 +41,18 @@ def makePrompt(sentence, codeLine, params1, preds1, promptNum):
 # 'matches' list.
 def getPrompts(matches, inputSentence):
     prompts = []
+    values = []
     matchCount = 0
     for match in matches:
         actionNum = match[0]
-        actionName = ls1[actionNum]
+        actionName = names[actionNum]
         annotNum = match[1]
         sentenceNum = match[2]
         matchValue = match[3]
-        actionName = ls1[actionNum]
+        actionName = names[actionNum]
         action = data.Data.actions[actionName]
         annots = action[1]
-        annot = sentenceProcessor.SentenceProcessor.tokenize(annots[annotNum])
+        annot = nltk.sent_tokenize(annots[annotNum])
         sentence = annot[sentenceNum]
         print(sentence)
         conditionValue = 0
@@ -73,48 +67,34 @@ def getPrompts(matches, inputSentence):
                 name = actionName + 'Effect'
                 sims = data.Data.similarities[actionName][1]
                 effectValue = (sims[annotNum])[sentenceNum]
+        value = 0.0
         if effectValue > conditionValue:
+            value = effectValue
             type = 'effect'
         else:
+            value = conditionValue
             type = 'condition'
+        values.append(value)
         actionCode = (data.Data.actions[actionName])[0]
         codeLine = actionCode[type]
         params1 = data.Data.params[actionName]
         preds1 = data.Data.preds[actionName]
-        prompt = makePrompt(sentence, inputSentence, codeLine, params1, [['pickle'], ['jar']], preds1, [['on top']], matchCount)
+        prompt = makePrompt(sentence, codeLine, params1, preds1, matchCount)
         prompts.append(prompt)
         matchCount += 1
+    avg = 0
+    if len(values) > 0:
+        avg = sum(values) / len(values)
+    print("AVG VALUE: "+str(avg))
+    if avg < 0.4:
+        prompts = []
     return prompts
 
-def findSimilarities(code, annots):
-    brokenEnglish = translate(code['condition'])
-    for i in range(0,5):
-        tokenizedAnnot = sentenceProcessor.SentenceProcessor.tokenize(annots[i])
-        sim = sentenceProcessor.SentenceProcessor.checkSim(brokenEnglish, tokenizedAnnot)
-        #print("Similarities:")
-        print(sim)
-        #print(sim)
-    return sim
-
-def findSimilarities2(inputText, annot, name):
+def findSimilarities(inputEmbedding, annot, name, actionNum, annotNum):
     annot = replaceParamsandPreds(annot, data.Data.params[name], data.Data.preds[name], 0)
-    tokenizedAnnot = sentenceProcessor.SentenceProcessor.tokenize(annot)
-    sim = sentenceProcessor.SentenceProcessor.checkSim(inputText, tokenizedAnnot)
-    #print("Similarities:")
-    print(sim)
-    #print(sim)
+    tokenizedAnnot = nltk.sent_tokenize(annot)
+    sim = sentenceProcessor.SentenceProcessor.checkSim(inputEmbedding, tokenizedAnnot, actionNum, annotNum)
     return sim
-
-def printSimilarity(actionName, lineName, annotName):
-    if actionName == 'loadTruck':
-        action = actions[0]
-        if lineName == 'condition':
-            codeLine = action.condition
-            if annotName == 'annot1Sims':
-                print(codeLine.annot1Sims)
-    #similarityLs = similarityDic[code][codeLine]
-    #similarityLs = similarityDic[codeLine]
-    #print(similarityLs)
 
 # Takes an input string representing a piece of natural language text,
 # a list of parameters and a list of predicates found in that text, 
@@ -142,7 +122,7 @@ def replaceReverse(result, params, preds):
     paramCount = 0
     for ls1 in params:
         for param in ls1:
-            result = result.replace("param0"+str(paramCount),param)
+            result = result.replace("param0"+str(paramCount)," "+param)
         paramCount += 1
     predCount = 0
     for ls2 in preds:
@@ -154,29 +134,56 @@ def replaceReverse(result, params, preds):
 # Takes a string representing the input and checks the similarity (cosine similarity) of the sentence
 # with all other sentences in the database. The location (indices) of the sentences with similarities 
 # above a certain threshold are stored in a list called 'matches', which is returned at the end.
-def testInputSim(inputSentence):
-    inputSentence = replaceParamsandPreds(inputSentence, [['pickle'], ['jar']], [['on top']], 0)
+def testInputSim(inputSentence, model):
+    input_embedding = model.encode([inputSentence])
     matches = []
-    for i in range(0,8):
-        name = ls1[i]
+    for i in range(0,num_actions):
+        name = names[i]
         ls = data.Data.actions[name]
         annots = ls[1]
         for j in range(0,5):
-            sims = findSimilarities2(inputSentence, annots[j], name)
+            sims = findSimilarities(input_embedding, annots[j], name, i, j)
             count = 0
             for simMetric in sims[0]:
-                if simMetric > 0.9:
+                if simMetric > 0.7:
                     matches.append([i, j, count, simMetric])
                 count += 1    
-    print("Matches: ")
-    print(matches)
     return matches
 
+# Example usage: printPDDL('movePickle', [['pickle'], ['jar']], [['top']], 5, ['(and (at start (on ?pickle ?jar))'], ['(and (at end (in ?pickle ?jar))'])
+def printPDDL(name, params, preds, duration, conditions, effects):
+    print("(:durative-action "+name)
+    print("    :parameters")
+    print("        (")
+    for ls in params:
+        print("        ?"+ls[0])
+    print("        )")
+    print("    :duration")
+    print("        (= ?duration " + duration + ")")
+    print("    :condition")
+    for condition in conditions:
+        print("        "+condition)
+    print("    :effect")
+    for effect in effects:
+        print("        "+effect)
+    print(")")
+
+# checks whether the sentence contains a word associated with a duration statement (i.e. a word in 'durationWords')
+def checkDuration(sentence):
+    found = False
+    for word in durationWords:
+        if word in sentence:
+            found = True
+    return found
+
 def main():
-    inputText = input("Type or paste in natural language that describes a durative action in PDDL.")
-    #inputText = "At the start, the pickle is on top of the jar."
+    model = SentenceTransformer('bert-base-nli-mean-tokens')
+    nlText = input("Type or paste in natural language that describes a durative action in PDDL.")
+    inputLs = nltk.sent_tokenize(nlText)
+    conditions = []
+    effects = []
     print("Here are some possible parameters:")
-    possibleParams = sentenceProcessor.SentenceProcessor.getPossibleParamsandPreds(inputText, 'params')
+    possibleParams = sentenceProcessor.SentenceProcessor.getPossibleParamsandPreds(nlText, 'params')
     count = 0
     for param in possibleParams:
         print(str(count)+": "+possibleParams[count])
@@ -197,8 +204,7 @@ def main():
     print("Parameter selection complete.")
     print("The parameters are: ")
     print(params)
-    #possiblePreds = ['on top', 'inside']
-    possiblePreds = sentenceProcessor.SentenceProcessor.getPossibleParamsandPreds(inputText, 'preds')
+    possiblePreds = sentenceProcessor.SentenceProcessor.getPossibleParamsandPreds(nlText, 'preds')
     print("Here are some possible predicates:")
     count = 0
     for pred in possiblePreds:
@@ -220,19 +226,43 @@ def main():
     print("Predicate selection complete.")
     print("The predicates are: ")
     print(preds)
-    print("Generating GPT3 prompt (this could take a few minutes).")
-    #params = input("Type or paste in natural language that describes a durative action in PDDL.")
-    #input = "At the start, the pickle is on top of the jar."
-    matches = testInputSim(inputText)
-    prompts = getPrompts(matches, inputText)
-    print("Use the following text as input to GPT3.")
-    for prompt in prompts:
-        print(prompt)
-    inputText = replaceParamsandPreds(inputText, params, preds, 0)
-    print(inputText)
-    print("\n")
-    result = input("Paste the first line of the GPT3 output here:")
-    fixedResult = replaceReverse(result, params, preds)
-    print(fixedResult)
-        
+    for inputText in inputLs:
+        if (checkDuration(inputText)):
+            for word in inputText.split():
+                modWord = word.replace(",","")
+                modWord = modWord.replace(".","")
+                if (modWord.isdigit()):
+                    duration = modWord
+        print("Generating GPT3 prompt (this could take a few minutes).")
+        inputText = replaceParamsandPreds(inputText, params, preds, 0)
+        matches = testInputSim(inputText, model)
+        prompts = getPrompts(matches, inputText)
+        if prompts:
+            print("Use the following text as input to GPT3.")
+            for prompt in prompts:
+                print(prompt)
+            #inputText = replaceParamsandPreds(inputText, params, preds, 0)
+            print(inputText)
+            print("\n")
+            result = input("Paste the first line of the GPT3 output here:")
+            result = replaceReverse(result, params, preds)
+            print("Here is the generated code for this sentence:")
+            print(result)
+            print("Select whether the above line of code is a condition or an effect.")
+            print("1: condition")
+            print("2: effect")
+            print("3: both")
+            print("4: neither")
+            type = input()
+            if (type == '1'):
+                conditions.append(result)
+            elif (type == '2'):
+                effects.append(result) 
+            elif (type == '3'):
+                pass
+            elif (type == '4'):
+                pass
+    name = input("What would you like to call this action?")
+    printPDDL(name, params, preds, duration, conditions, effects)   
+
 main()
