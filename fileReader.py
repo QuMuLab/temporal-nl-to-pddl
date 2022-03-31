72c52043258a705851b5d00b95e9b8829533b2a0
beamisha <beamish.alex@gmail.com>
2022-03-30 22:21:19 -0400

    Update fileReader.py

--- fileReader.py
+++ fileReader.py
@@ -1,42 +1,29 @@
-import json
-import nltk
-import sys
-import keywords
-from sentenceProcessor import SentenceProcessor
-from display import Display
+import pickle
 
-# Read annotations from a single text file and return the annotations
-def getAnnotations(filePath):
-    file = open(filePath)
-    annot = []
-    annot.append(file.readline(-1))
-    for i in range(0,6):
-        blank = file.readline(-1)
-        annot.append(file.readline(-1))
-    file.close()
-    return annot
+class FileReader:
 
-# Use NLTK to tokenize (break up into a list of sentences) each annotation
-def tokenize(annot):
-    for i in range(0,6):
-        print("Next line:")
-        print(annot[i])
-        annot[i] = nltk.sent_tokenize(annot[i])
-        print(annot[i])
-    return annot
+    def getObj(fileName):
+        path = 'Desktop/TempNLtoPDDL/Data/' + fileName + '.xml'
+        file = open(path, 'rb')
+        obj = pickle.load(file)
+        return obj
 
-# Read and tokenize annotations from text file, create sentence objects with links between 
-# similar sentences
-def fileTest(filePath):
-    annot = tokenize(getAnnotations(filePath))
-    similarityDic = SentenceProcessor.createSimilarityDic(annot)
-    print("SIMILARITY DIC:")
-    Display.printDic(similarityDic)
-    indexDic = SentenceProcessor.getGroups(similarityDic)
-    print("INDEX DIC:")
-    Display.printDic(indexDic)
-    sentences = SentenceProcessor.createSentenceObjects(indexDic, 0)
-    #Display.printSentenceDic(sentences)
-    #SentenceProcessor.createReverseLinks(sentences)
-    #Display.printSentenceDic(sentences)
-    return(sentences)
+    def getAnnot(num):
+        fileName = 'annot' + str(num)
+        return FileReader.getObj(fileName)
+        
+    def getActions():
+        fileName = 'actions'
+        return FileReader.getObj(fileName)
+
+    def getParams():
+        fileName = 'params'
+        return FileReader.getObj(fileName)
+
+    def getPreds():
+        fileName = 'preds'
+        return FileReader.getObj(fileName)
+
+    def getSimilarities():
+        fileName = 'similarities'
+        return FileReader.getObj(fileName)
